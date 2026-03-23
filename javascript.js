b92230a97749e235a3649ba234d57b7a397ba5c5
Mohamed-Sobhy-2004 <117641777+Mohamed-Sobhy-2004@users.noreply.github.com>
2026-03-23 22:30:08 +0200

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,35 @@
 let up = document.querySelector('.up')
 
+function sendEmail(){
+    const data ={
+        To_email:'mhmdssssn64@gmail.com',
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        sub: 'Service',
+        message:`
+        Name : ${document.getElementById('name').value}
+        Email : ${document.getElementById('email').value}
+        Phone Number : ${document.getElementById('Phone').value}
+        Message : ${document.getElementById('message').value}
+        `
+    }
+    emailjs.send('service_zmgwepn','template_gxabvb2',data).then((Response) =>{
+        alert('Success')
+    }).catch((Error) =>{
+        alert(Error)
+    })
+}
+
+
+document.getElementById('send').addEventListener('click',() =>{
+    if(document.getElementById('email').value != '' && document.getElementById('message').value !=''){
+        sendEmail()
+    }else{
+        alert('Pleas Enter your Email And your Message')
+    }
+})
+
+
 onscroll = function(){
     if(scrollY >= 362){
         up.style.opacity = '1'
